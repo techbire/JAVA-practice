b9e54ad14366089fe57ff6931701cadbbdd2f263
techbire <anshtechnical@gmail.com>
2025-01-15 11:08:52 +0530

    changes

--- Introduction/Main.java
+++ Introduction/Main.java
@@ -1,6 +1,14 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    for(x=0;x<9;x++){
+        System.out.println("the value of x: "+x);
+    }   
     }
 }
 
