412607be1920b3e2493fd5ccc963b1225f66fd13
techbire <anshtechnical@gmail.com>
2025-01-15 11:17:54 +0530

    added

--- Introduction/Main.java
+++ Introduction/Main.java
@@ -4,11 +4,21 @@
 //     }
 // }
 
+// public class Main {
+//     public static void main(String[] args) {
+//     for(x=0;x<9;x++){
+//         System.out.println("the value of x: "+x);
+//     }   
+//     }
+// }
+
 public class Main {
     public static void main(String[] args) {
-    for(x=0;x<9;x++){
-        System.out.println("the value of x: "+x);
-    }   
+    int x=4;
+    int y=3;
+    if(x>y){System.out.println("x is bigger");}
+    y++;
+    x--;
+    if(y>x){System.out.println("y is bigger");}   
     }
 }
-
